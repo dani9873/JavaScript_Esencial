72da2e1763021070cafdd1ca73e0b49fd2f5bac9
dani9873 <danielescobar.987@gmail.com>
2023-08-25 00:20:34 -0600

    Primeros avances

---
+++ 1 Capitulo/clase2.js
@@ -0,0 +1,15 @@
+var nombre = 'Sacha', apellido = 'Lifszyc'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+//var nombreCompleto = nombre + ' '+ apellido
+// codigo mas moderno
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+//var str = nombre.charAt(1) + nombre.charAt(2) 
+var str = nombre.substr(1,2)
+var ultimaLetraNombre = nombre.substr(-1)
